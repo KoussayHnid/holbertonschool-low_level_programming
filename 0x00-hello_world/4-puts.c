1daade72c69a5feac04ee3926b30133a95d31045
root <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>
2021-09-30 03:54:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-printf( "Welcome to C!\n" );  
+#include <stdio.h>
+/**
+ * main - print message
+ * Return: return int
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
