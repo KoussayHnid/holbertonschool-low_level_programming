013881cd6285a0e3b03d0badf02e4ced6523f596
root <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>
2021-10-05 05:30:12 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *int _isalpha-checks for alphabetic character
+ *@n:first letter 
+ *
+ * Return: 0
+ */
+ int _isalpha(int n)
+{
+	if ((n >= 65 && n <= 90) || (n >= 97 && n <= 122))
+	return (1);
+	else
+	return (0);
+}
