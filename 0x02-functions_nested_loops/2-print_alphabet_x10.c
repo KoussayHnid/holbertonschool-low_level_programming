91b3f047a4a23ddcac25394449e6791041606f3b
root <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>
2021-10-05 04:46:42 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
-        /**
-        * main - print 10 times the alphabet
-        *
-        * Return: 0
-        */
+/**
+* main - print 10 times the alphabet
+*
+* Return: 0
+*/
 void print_alphabet_x10(void)
 {
         int i;
