6636c9f3514f9004d80b99e605460ef8f1beb65a
root <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>
2021-10-05 04:17:21 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+	{
+putchar(i);
+	}
+	putchar('\n');
+}
