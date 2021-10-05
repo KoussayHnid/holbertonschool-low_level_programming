d83596430953ec5c135b9301589e949d0041da5e
root <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>
2021-10-05 04:54:25 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 */
 void print_alphabet_x10(void)
 {
-        int i;
-        int n = 0;
+	int i;
+	int n = 0;
 
         while (n < 10)
-        {
-                for (i = 'a'; i <= 'z'; i++)
-                        putchar(i);
-                putchar('\n');
-                n++;
-        }
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			putchar(i);
+		putchar('\n');
+		n++;
+	}
 }
