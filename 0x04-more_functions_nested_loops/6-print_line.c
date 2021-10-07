3407b61de0cf073b261270ae127cfb7cc1b634b7
root <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>
2021-10-07 04:59:40 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - Draws a straight line using the character _.
+ * print_line - Draws a straight line using the character
  * @n: The number of _ characters to be printed.
  */
 void print_line(int n)
@@ -10,8 +10,7 @@ void print_line(int n)
 	if (n > 0)
 	{
 	for (len = 0; len < n; len++)
-		putchar('');
+	putchar('');
 	}
-
 	_putchar('\n');
 }
