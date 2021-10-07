91a181a9c08bcef955576cb7f6e21e6455b911ad
root <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>
2021-10-07 01:53:15 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - causes an infinite loop
+* main -  infinite loop
 * Return: 0
 */
 int main(void)
@@ -10,15 +10,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
-
-/**	
+/**	i = 0;
+*
 *	 while (i < 10)
 *	{
 *		putchar(i);
 *	}
 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
