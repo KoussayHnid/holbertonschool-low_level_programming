956631ab9d057683f8d798dab8a9f740629cf336
root <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>
2021-10-11 02:30:06 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * reset_to_98 - takes a pointer to an int and updates it
- * @n: pointer
+ * @n: int
  *
  * Return: value
  */
 void reset_to_98(int *n);
 {
-	*n = 98;
+*n = 98;
 }
