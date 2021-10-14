146da8cab4044dfa22f18e1d92f417aac49a07c9
root <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>
2021-10-14 04:20:36 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **cap_string - capitalizes all words of a string
+ *@str: char
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+		str[i] = str[i] - 32;
+		}
+	}
+	return str;
+}
