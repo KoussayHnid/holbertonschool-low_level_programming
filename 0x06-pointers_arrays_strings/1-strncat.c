4e57dd88c45d0cbc0d95847fa04543003741e643
root <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>
2021-10-14 05:15:03 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,13 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0')
+	while (*dest[i] != '\0')
 	{
 		i++;
 	}
-	while (j < n && src[j] != '\0')
+	while (j < n && *src[j] != '\0')
 	{
-		dest[i] = src[j];
+		*dest[i] = *src[j];
 		j++;
 		i++;
 	}
