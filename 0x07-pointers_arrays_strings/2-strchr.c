6f7c722c1267bee1f059d73bacac2fa6bed6c946
root <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>
2021-10-19 05:49:28 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - function with two arguments
+ * @s: char
+ * @c: char
+ *
+ * Return: character in string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
