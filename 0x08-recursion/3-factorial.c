28aae0b020b3bebd6bc9796aa4c3be751e2da3f5
root <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>
2021-10-20 12:29:12 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- *factorial(int n) -  returns the factorial of a given number
- *@n: int
+ *factorial -  returns the factorial of a given number
+ *
+ * @n: int
  *
  *Return: n
  */
 int factorial(int n)
 {
+	int n;
+
 	if (n == 0)
 		return (1);
 	else if (n < 0)
