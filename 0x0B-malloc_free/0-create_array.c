e5e4903e7471ea7e04b4d2b6004fadf17eaa7ac4
root <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>
2021-10-26 02:07:33 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * *create_array - creates an array of chars, and initializes it with a specific char
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
  * @size: int
  * @c: char
  *
