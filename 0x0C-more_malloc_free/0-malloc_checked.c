944d825be843ab981917a2caa391ff0bb1288e8b
root <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>
2021-10-28 04:39:01 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ **malloc_checked - that allocates memory using malloc.
+ *@b: unsigned int
+ *
+ *Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+
+}
