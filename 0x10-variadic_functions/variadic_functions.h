ea40b6e3e4d511544de7cbd3dcb5114246073cac
root <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>
2021-11-04 13:51:11 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTION.H
+#define VARIADIC_FUNCTION.H
+
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
