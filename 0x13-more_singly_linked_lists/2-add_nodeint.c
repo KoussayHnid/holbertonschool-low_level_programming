c112ca0f979b1d8f1aca87a60360e960d656b53b
root <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>
2021-11-16 01:46:30 -0800

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ **add_nodeint - that adds a new node at the beginning of a listint_t list.
+ *@head: pointer
+ *@n: value
+ *
+ *Return: temp 
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
