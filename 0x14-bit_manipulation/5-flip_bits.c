24667224ab7e8686aa825968f2a459e88dfc043c
root <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>
2021-11-18 09:38:35 -0800

    task5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - that returns the number of bits you would need to flip 
+ * flip_bits - that returns the number of bits you would need to flip
  * to get from one number to another.
  * @n: int
  * @m: int
