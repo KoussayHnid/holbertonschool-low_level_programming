7cd37ffb48cfd804c7a28df338a7d5fdfc8d1c6b
root <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>
2022-01-06 04:40:46 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_dlistint - that prints all the elements of a dlistint_t list
- * @h: pointer 
+ * @h: pointe
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
